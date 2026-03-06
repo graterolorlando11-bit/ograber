8cef338d0c5edb1cf757f3fb89429725e1758470
joamona <joamona@cgf.upv.es>
2026-03-06 13:17:41 +0100

    Added geometry checks to the insert script with django models

--- djangoapi/buildings2/models.py
+++ djangoapi/buildings2/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.gis.db import models as gis_models
+import django.utils.timezone as djangoTimezone
 
 # Create your models here.
 class Buildings(models.Model):
@@ -8,5 +9,12 @@ class Buildings(models.Model):
     perimeter = models.FloatField(blank=True, null=True)
     height = models.FloatField(blank=True, null=True)
     geom = gis_models.PolygonField(srid=25830,blank=True, null=True) 
-    class meta:
-        manage = False
+    data_creation = models.DateTimeField(blank = True, db_default=djangoTimezone.now())
+
+    def save(self, *args, **kwargs):
+            # Calculate values from the geometry before saving
+            if self.geom:
+                self.area = self.geom.area
+                self.perimeter = self.geom.length
+            
+            super().save(*args, **kwargs)
